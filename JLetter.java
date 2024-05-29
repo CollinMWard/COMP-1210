3a200e1bf4a15d97edf32708edfcaf2b5df6ec12
CollinMWard <105256424+CollinMWard@users.noreply.github.com>
2024-05-28 19:00:56 -0500

    Add files via upload

---
+++ JLetter.java
@@ -0,0 +1,28 @@
+/** 
+   *  A simple program to print a large J.
+   * 
+   * Project 1
+   * Collin Ward – COMP-1210 - 001
+   * 8/24/22
+   */ 
+public class JLetter
+{
+  /** 
+   * Prints a large J.
+   * @param args Command line arguments – not used.
+   */ 
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   
+   }
+}
---
+++ MyLifeGoals.java
@@ -0,0 +1,28 @@
+/** 
+   *  A simple program to print my short-term, medium-term, and long-term goals.
+   * 
+   * Project 1
+   * Collin Ward – COMP-1210 - 001
+   * 8/24/22
+   */ 
+public class MyLifeGoals
+{
+  /** 
+   * Prints my name and life goals.
+   * @param args Command line arguments – not used.
+   */ 
+   public static void main(String[] args)
+   {
+      System.out.println("My name is Collin McCreary Ward.");
+      System.out.println();
+      System.out.println("My short-term goals include maintaining my GPA, "
+         + "acquiring internships, and networking.");
+      System.out.println("My medium-term goals include learning multiple "
+         + "languages, writing advanced programs, "
+         + "starting new partnerships, and devloping my skills.");
+      System.out.println("My long-term goals include "
+         + "graduating, attending a graduate program,"
+         + " and devloping a successful business.");
+   
+   }
+}
